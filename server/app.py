91563fa2858f8773eb86d7b3b244191171c77acd
YASHcode-IIITV <yashrajsiwal@gmail.com>
2026-04-12 21:23:16 +0530

    Fix port conflict in inference.py

--- server/app.py
+++ server/app.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
 
 def main():
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=7860)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
 
 if __name__ == '__main__':
     main()
